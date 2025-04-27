c26dbff7bc8dfd599d6b28f156bf425e97da8f7c
qngu0461 <qngu0461@uni.sydney.edu.au>
2025-04-28 04:21:27 +1000

    Update building-info.php

--- api/building-info.php
+++ api/building-info.php
@@ -1,4 +1,8 @@
 <?php
+// /api/documents.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
 
 $host = 'ep-late-shadow-a7xntzmh-pooler.ap-southeast-2.aws.neon.tech';
--- api/documents.php
+++ api/documents.php
@@ -1,5 +1,8 @@
 <?php
 // /api/documents.php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 // Handle preflight CORS request
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
